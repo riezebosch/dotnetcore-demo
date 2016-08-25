6efea8cc46174da096fded5dbd1c5197913ad4c9
Manuel Riezebosch <mriezebosch@gmail.com>
2016-08-25 15:25:58 +0200

    communicatie tussen mvc frontend en webapi backend
    client gegenereerd met NSwagStudio
    deze installeer je met `choco install nswagstudio -y`

--- src/mvc-demo/Controllers/HomeController.cs
+++ src/mvc-demo/Controllers/HomeController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using mvc_demo.Models.HomeViewModels;
 
 namespace mvc_demo.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var client = new Services.Client("http://localhost:1889/");
+            var result = await client.ApiValuesGetAsync();
+
+            return View(new IndexViewModel { Values = result });
         }
 
         public IActionResult About()
---
+++ src/mvc-demo/Models/HomeViewModels/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace mvc_demo.Models.HomeViewModels
+{
+    public class IndexViewModel
+    {
+        public IList<string> Values { get; set; }
+    }
+}
