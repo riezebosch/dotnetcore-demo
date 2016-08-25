d321e7b85df47a19861fa0bff4994544e3d5731c
Manuel Riezebosch <mriezebosch@gmail.com>
2016-08-25 14:53:02 +0200

    xunit toegevoegd en werkend

---
+++ test/ef-demo.tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ef_demo.tests
+{
+    /// <summary>
+    /// With the help of: https://xunit.github.io/docs/getting-started-dotnet-core.html
+    /// </summary>
+    public class Class1
+    {
+        [Fact]
+        public void TestEFCoreCreatesSqliteDatabase()
+        {
+
+        }
+    }
+}
