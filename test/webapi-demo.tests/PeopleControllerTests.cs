91b0c52a70b94a2b15f2b3eb1b9df29b9382bd36
Manuel Riezebosch <mriezebosch@gmail.com>
2016-08-25 15:35:37 +0200

    test project voor webapi-demo

---
+++ test/webapi-demo.tests/PeopleControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace webapi_demo.tests
+{
+    public class PeopleControllerTests
+    {
+        [Fact]
+        public void TestGetReturnsListOfPeople()
+        {
+
+        }
+    }
+}
