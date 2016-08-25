3eb8c67efa545011680614a567f438224fef8dbd
Manuel Riezebosch <mriezebosch@gmail.com>
2016-08-25 15:54:55 +0200

    PeopleController met in-memory unit test

---
+++ src/webapi-demo/Controllers/PeopleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PeopleController
+{
+    private DemoContext context;
+
+    public PeopleController(DemoContext context)
+    {
+        this.context = context;
+    }
+
+    public IList<Person> Get()
+    {
+        return context.People.ToList(); ;
+    }
+}
--- test/webapi-demo.tests/PeopleControllerTests.cs
+++ test/webapi-demo.tests/PeopleControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,9 +10,30 @@ namespace webapi_demo.tests
     public class PeopleControllerTests
     {
         [Fact]
-        public void TestGetReturnsListOfPeople()
+        public async Task TestGetReturnsListOfPeople()
         {
+            using (var context = await InitializeDatabase())
+            {
+                var controller = new PeopleController(context);
+                var result = controller.Get();
 
+                Assert.Contains<Person>(result, p => p.FirstName == "Manuel" && p.LastName == "Riezebosch");
+            }
+        }
+
+        private static async Task<DemoContext> InitializeDatabase()
+        {
+            var builder = new DbContextOptionsBuilder<DemoContext>();
+            builder.UseSqlite("FileName=./test.db");
+
+            var context = new DemoContext(builder.Options);
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            context.People.Add(new Person { Id = 0, FirstName = "Manuel", LastName = "Riezebosch" });
+            await context.SaveChangesAsync();
+
+            return context;
         }
     }
 }
