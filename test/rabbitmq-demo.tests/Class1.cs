e18ad6ad16d355e5b44b0404cc8ae8ccf8e6f052
Manuel Riezebosch <mriezebosch@gmail.com>
2016-10-25 21:12:45 +0200

    eerste stukje van tutorial: verzenden van data maar dan in unit test

--- test/rabbitmq-demo.tests/Class1.cs
+++ test/rabbitmq-demo.tests/Class1.cs
@@ -1,14 +1,37 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace rabbitmq_demo.tests
 {
     public class Class1
     {
-        public Class1()
+        [Fact]
+        public void SendMessageShouldBeReceived()
         {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+
+            }
         }
     }
 }
