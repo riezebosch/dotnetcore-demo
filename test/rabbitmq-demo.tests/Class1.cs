282996e9b2f4fd04c791f05e4557c0bf4e5d24e0
Manuel Riezebosch <mriezebosch@gmail.com>
2016-10-25 21:19:17 +0200

    werkend testje met send/receive van rabbitmq mbv autoresetevent

--- test/rabbitmq-demo.tests/Class1.cs
+++ test/rabbitmq-demo.tests/Class1.cs
@@ -1,8 +1,10 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -13,6 +15,7 @@ namespace rabbitmq_demo.tests
         [Fact]
         public void SendMessageShouldBeReceived()
         {
+            // Arrange
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
@@ -30,7 +33,36 @@ namespace rabbitmq_demo.tests
                                      routingKey: "hello",
                                      basicProperties: null,
                                      body: body);
+            }
 
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                using (var wait = new AutoResetEvent(false))
+                {
+                    string message = null;
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        message = Encoding.UTF8.GetString(body);
+
+                        wait.Set();
+                    };
+
+                    channel.BasicConsume(queue: "hello",
+                                         noAck: true,
+                                         consumer: consumer);
+
+                    Assert.True(wait.WaitOne(TimeSpan.FromSeconds(30)));
+                    Assert.Equal("Hello World!", message);
+                }
             }
         }
     }
