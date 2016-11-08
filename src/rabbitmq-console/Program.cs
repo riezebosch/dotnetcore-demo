4e32ea430f880169731a1025db7837b282058325
Manuel Riezebosch <mriezebosch@gmail.com>
2016-11-08 12:56:57 +0100

    console voor chatten via RabbitMQ

---
+++ src/rabbitmq-console/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using rabbitmq_demo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rabbitmq_console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connection = new ConnectionFactory
+            {
+                HostName = "curistm01",
+                UserName = "manuel",
+                Password = "manuel"
+            };
+
+            using (var receiver = new Receiver(connection, "rabbitmq-demo"))
+            using (var sender = new Sender(connection, exchange: "rabbitmq-demo"))
+            {
+                receiver.Subscribe<string>(Console.WriteLine);
+
+                string input = string.Empty;
+                while ((input = Console.ReadLine()) != "exit")
+                {
+                    sender.Publish(input);
+                }
+            }
+        }
+    }
+}
---
+++ src/rabbitmq-console/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rabbitmq_console")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0359c6e4-ef6c-41b9-9d90-ba7b16ad2d6c")]
