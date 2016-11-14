29ce9a552fa245ab99385f27287bb9a988799213
Manuel Riezebosch <mriezebosch@gmail.com>
2016-11-14 11:20:03 +0100

    files op juiste plek

---
+++ test/rabbitmq-demo.tests/Person.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace rabbitmq_demo.tests
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
